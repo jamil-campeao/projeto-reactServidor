b1adcdc130d5692b99aafdb4bd5e3bf98c2ce4fa
Jamil Campeão <jamilcampeao@gmail.com>
2024-12-14 20:07:31 -0300

    Criado pasta de controladores

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-const express = require("express");
+const express = require('express');
+const rotaLivro = require('./rotas/livro');
+const app = express();
+const port = 8000;
+app.use('/livros', rotaLivro);
+
+app.listen(port, () => {
+    console.log(`Escutando a porta ${port}`);
+})
