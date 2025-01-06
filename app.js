b1517e6d7c3d0a5735610d6a8f7e49dc31c5efb1
Jamil Campeão <jamilcampeao@gmail.com>
2025-01-05 23:33:55 -0300

    Ajustes no arquivo de Private Key

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+require('dotenv').config();
 const express = require('express');
+const serverless = require('serverless-http');
 const cors = require('cors');
+const { db } = require('./firebaseAdmin');
 
 // Importando as rotas
 const rotaLivro = require('./rotas/livro');
@@ -8,16 +11,20 @@ const rotaCategoria = require('./rotas/categoria');
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: '*' }));
+app.use(cors({ origin: '*' })); // Habilita o CORS para todas as origens
 
 // Registrando as rotas
 app.use('/livros', rotaLivro);
 app.use('/favoritos', rotaLivroFavorito);
 app.use('/categorias', rotaCategoria);
 
-// Porta para testes locais
-const PORT = process.env.PORT || 8000;
+// Para testes locais
+if (!process.env.NETLIFY) {
+    const PORT = process.env.PORT || 8000;
+    app.listen(PORT, () => {
+        console.log(`Servidor rodando localmente na porta ${PORT}`);
+    });
+}
 
-app.listen(PORT, () => {
-    console.log(`Servidor rodando localmente na porta ${PORT}`);
-});
+// Exporta o app para o Netlify
+module.exports.handler = serverless(app);
--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,13 +1,16 @@
 const admin = require('firebase-admin');
 
+if (!process.env.FIREBASE_PROJECT_ID || !process.env.FIREBASE_PRIVATE_KEY || !process.env.FIREBASE_CLIENT_EMAIL) {
+    throw new Error('As variáveis de ambiente do Firebase não estão configuradas corretamente.');
+}
+
 admin.initializeApp({
     credential: admin.credential.cert({
         projectId: process.env.FIREBASE_PROJECT_ID,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Corrige quebras de linha
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     }),
 });
-
 const db = admin.firestore();
 
 module.exports = { db };
